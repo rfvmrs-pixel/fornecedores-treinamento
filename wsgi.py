aae23efb76c834f9ca769570c42b5a788defd638
rfvmrs-pixel <rfvmrs@gmail.com>
2026-09-11 17:12:11 -0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""Ponto de entrada WSGI para servidores de produção (gunicorn, etc.).
-
-Exemplo (Procfile / Render / Railway):
-    gunicorn wsgi:app
-"""
-from app import create_app
-
-app = create_app()
